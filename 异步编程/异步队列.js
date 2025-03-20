6b284665f231c4be8d9dd5a4f8af91d0c4fe3709
Cao007 <1114685490@qq.com>
2025-03-21 02:05:59 +0800

    异步队列

---
+++ 异步编程/异步队列.js
@@ -0,0 +1,21 @@
+console.log(1);
+let a = 2;
+setTimeout(() => {
+  console.log("setTimeout1");
+  new Promise((resolve) => {
+    resolve();
+  }).then(() => {
+    console.log("then2");
+  });
+});
+console.log(a);
+new Promise((resolve) => {
+  console.log("promise1");
+  setTimeout(() => {
+    console.log("setTimeout2");
+  });
+  resolve();
+}).then(() => {
+  console.log("then1");
+});
+console.log(3);
