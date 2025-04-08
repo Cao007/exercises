470768e514bfff087dc49f9d3e5f26b126aa40a1
Cao007 <1114685490@qq.com>
2025-04-08 16:48:19 +0800

    feat: ts

---
+++ ts/dist/1.基本数据类型.js
@@ -0,0 +1,8 @@
+"use strict";
+const a = 1;
+const b = "2";
+const c = true;
+const d = null;
+const e = undefined;
+const f = Symbol("foo");
+const g = 123n;
---
+++ ts/src/1.基本数据类型.ts
@@ -0,0 +1,7 @@
+const a: number = 1;
+const b: string = "2";
+const c: boolean = true;
+const d: null = null;
+const e: undefined = undefined;
+const f: Symbol = Symbol("foo");
+const g: bigint = 123n;
